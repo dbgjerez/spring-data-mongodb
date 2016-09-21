b5d7c1f084069ccb4f1f7d55f7ddcfb11b48d501
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-09-21 23:39:44 +0200

    anotación para autoconfigurar la aplicación

--- src/main/java/com/dbg/mongo/SpringDataMongodbApplication.java
+++ src/main/java/com/dbg/mongo/SpringDataMongodbApplication.java
@@ -1,12 +1,15 @@
-package com.dbg;
+package com.dbg.mongo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class SpringDataMongodbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataMongodbApplication.class, args);
 	}
+
 }
