4ada22e28ce0131ec623888e4693ae75cc811a39
David Borrego Gutiérrez <dborrego@atsistemas.com>
2016-09-21 23:39:12 +0200

    clase para representar autores

---
+++ src/main/java/com/dbg/mongo/model/Author.java
@@ -0,0 +1,45 @@
+package com.dbg.mongo.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class Author implements Serializable {
+
+	private static final long serialVersionUID = -1938192829120705729L;
+
+	private ObjectId id;
+
+	private String name;
+
+	private List<Book> books = new ArrayList<>();
+
+	public ObjectId getId() {
+		return id;
+	}
+
+	public void setId(ObjectId id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<Book> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+}
